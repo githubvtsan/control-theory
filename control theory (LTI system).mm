b6fdebc71321149bf8604fc6b48840e48a5c87e1
githubvtsan <Vi-hung@hotmail.com>
2018-11-26 00:48:17 +0100

    updated

---
+++ control theory (LTI system).mm
@@ -0,0 +1,331 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="control theory (LTI system)" FOLDED="false" ID="ID_1802145161" CREATED="1540467403196" MODIFIED="1540473876440" STYLE="oval">
+<font NAME="Calibri" SIZE="12"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="types of control" POSITION="right" ID="ID_571742614" CREATED="1540467413023" MODIFIED="1540473876450">
+<edge COLOR="#ff0000"/>
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="passive" ID="ID_49159327" CREATED="1540468612556" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="spoiler" ID="ID_1473579425" CREATED="1540468660763" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="minimize drag" ID="ID_1479160470" CREATED="1540468689172" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="no energy needed" ID="ID_1338595698" CREATED="1540468670573" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="active" ID="ID_1198035085" CREATED="1540468619737" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="energy required" ID="ID_1294134650" CREATED="1540468709553" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="open loop" ID="ID_1457905437" CREATED="1540468720487" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="preplanned input" ID="ID_483160159" CREATED="1540468747079" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="drawbacks" ID="ID_1813092404" CREATED="1540468762870" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="not energy efficient" ID="ID_119137665" CREATED="1540468776412" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="can not deal with uncertainty" ID="ID_1610152213" CREATED="1540468778332" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="stability" ID="ID_1010127895" CREATED="1540468779583" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="can not fundamentally change system" ID="ID_1310293695" CREATED="1540468877399" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="do not reject disturbances" ID="ID_279022297" CREATED="1540468780502" MODIFIED="1540473876450">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="closed loop" ID="ID_322963896" CREATED="1540468727572" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="sensors used" ID="ID_1757824195" CREATED="1540469032042" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="pros" ID="ID_1870339324" CREATED="1540469044128" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="reject disturbances" ID="ID_501694624" CREATED="1540469066040" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="external" ID="ID_1114801878" CREATED="1540469135834" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="less energy needed" ID="ID_1747089650" CREATED="1540469151050" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="deal with uncertainty" ID="ID_1700688055" CREATED="1540469068093" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="change system dynamics fundamentally" ID="ID_1692821411" CREATED="1540469075177" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="" ID="ID_1780706336" CREATED="1540469204438" MODIFIED="1543184552227">
+<font NAME="Calibri" SIZE="12"/>
+<hook URI="feedback%20change%20dynamics.jpg" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="" ID="ID_778178376" CREATED="1540469509097" MODIFIED="1543184577915">
+<font NAME="Calibri" SIZE="12"/>
+<hook URI="feedback%20change%20dynamics%20proof.jpg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="system description" POSITION="right" ID="ID_1868221547" CREATED="1540467985362" MODIFIED="1540473876460">
+<edge COLOR="#00ff00"/>
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="state space" ID="ID_1606977410" CREATED="1540469617802" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="" ID="ID_829161432" CREATED="1540469661341" MODIFIED="1543184608921">
+<font NAME="Calibri" SIZE="12"/>
+<hook URI="state%20space.jpg" SIZE="0.81120574" NAME="ExternalObject"/>
+</node>
+<node TEXT="U" ID="ID_423049092" CREATED="1540469663036" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="The actuator we can control" ID="ID_531988973" CREATED="1540469743232" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="B" ID="ID_889864738" CREATED="1540469674006" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="tells how U affects the change in time of system" ID="ID_1785692516" CREATED="1540469765688" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="system state" POSITION="left" ID="ID_117610802" CREATED="1540469807416" MODIFIED="1540473876460">
+<edge COLOR="#ff00ff"/>
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex solution to $\dot x = Ax$" ID="ID_1159354896" CREATED="1540469831142" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex x(t) = e^{At}x(0)" ID="ID_1682238315" CREATED="1540470136011" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex e^{At}" ID="ID_476095454" CREATED="1540470236348" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="called matrix exponential" ID="ID_1210510267" CREATED="1540470252765" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="taylor expansion" ID="ID_769684660" CREATED="1540470253946" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="general (around 0)" ID="ID_106481329" CREATED="1540470300282" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex $e^x = 1+x + \frac{x^2}{2!} + \frac{x^3}{3!} + \frac{x^4}{4!} + ... = \sum_{i=0}^\infty \frac{x^i}{i!}$" ID="ID_1361493977" CREATED="1540470434572" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="this case (around 0)" ID="ID_51653259" CREATED="1540470303121" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex $e^{At} = I + At+ \frac{A^2t^2}{2!} + ...  = \sum_{k=0}^{\infty}\frac{A^kt^k}{k!}$" ID="ID_631906770" CREATED="1540471091937" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex using $A = S^{-1}\lambda S$" ID="ID_1337188029" CREATED="1540474080198" MODIFIED="1540474113162">
+<node TEXT="\latex $e^{S\lambda S^{-1}t} = I + S\lambda S^{-1} t + \frac{S\lambda S^{-1} S\lambda S^{-1} t^2}{2!} + ...$" ID="ID_1314451676" CREATED="1540474124052" MODIFIED="1540474933642">
+<node TEXT="\latex note that SS^{-1} = I" ID="ID_1130889403" CREATED="1540474393002" MODIFIED="1540474421324">
+<node TEXT="\latex so  $e^{S\lambda S^{-1} t} = I + S\lambda S^{-1} t + \frac{S\lambda^2 S^{-1} t^2}{2!} + ...$" ID="ID_715674957" CREATED="1540474432072" MODIFIED="1540474648785">
+<node TEXT="\latex S[I + \lambda t + \frac{\lambda^2 t^2}{2!} + ... ]S^{-1}" ID="ID_886304307" CREATED="1540474828020" MODIFIED="1540475421726">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_631906770" STARTINCLINATION="-76;-298;" ENDINCLINATION="640;-64;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="\latex e^{At} = Se^{\lambda t}S^{-1}" ID="ID_203560086" CREATED="1540475090496" MODIFIED="1540475161770">
+<node TEXT="will not change with complex roots" ID="ID_1076099368" CREATED="1540480954247" MODIFIED="1540481018133" LINK="http://tutorial.math.lamar.edu/Classes/DE/ComplexRoots.aspx"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="not practical" ID="ID_594008176" CREATED="1540470254556" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="using eigenvectors and eigenvalues" ID="ID_1286358564" CREATED="1540471320704" MODIFIED="1540473876460">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="state coordinate transformation" ID="ID_1907728881" CREATED="1540471958287" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="" ID="ID_936806799" CREATED="1540472054446" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node ID="ID_1206261031" CREATED="1540472073693" MODIFIED="1540473879481"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      map to eigenvector coordinate system
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="easier calculations" ID="ID_505834497" CREATED="1540472079276" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="dynamics decoupled" ID="ID_447585507" CREATED="1540475664947" MODIFIED="1540475673705"/>
+</node>
+<node TEXT="\latex z = Tx \xrightarrow{} x = T^{-1}z = Sz" ID="ID_817042645" CREATED="1540472092002" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="T" ID="ID_1674417731" CREATED="1540472112766" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="" ID="ID_1854741682" CREATED="1540472365452" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="T is a abritrary non-singular matrix" ID="ID_377888008" CREATED="1540472112766" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="pivot position in every row" ID="ID_856849762" CREATED="1540472648776" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+</node>
+<node TEXT="\latex in this course $T = S^{-1}$" ID="ID_386429212" CREATED="1540472367308" MODIFIED="1543189518662">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_303299790" STARTINCLINATION="490;-220;" ENDINCLINATION="494;-75;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+<node TEXT="\latex \dot x = S\dot z = Ax" ID="ID_537429784" CREATED="1540472165969" MODIFIED="1540473879491">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex \dot z = S^{-1}Ax = S^{-1}ASz" ID="ID_935495340" CREATED="1540472969632" MODIFIED="1540473879491">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1877093986" STARTINCLINATION="185;0;" ENDINCLINATION="185;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex \dot z = \lambda z" ID="ID_1620949997" CREATED="1540473090983" MODIFIED="1540473879491">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="so now the diagonal matrix simplifies it" ID="ID_934417701" CREATED="1540473688687" MODIFIED="1540473903972">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="solution is" ID="ID_65235315" CREATED="1540473689598" MODIFIED="1540473879491">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex z = e^{\lambda}z(0)" ID="ID_1847842358" CREATED="1540473767529" MODIFIED="1540473879471">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="\latex Av = \lambda v" ID="ID_526827586" CREATED="1540471362253" MODIFIED="1540473879471">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="diagonizable A" ID="ID_1319053732" CREATED="1540471493576" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex \lambda" ID="ID_348160909" CREATED="1540471584355" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="is diagonal" ID="ID_677988915" CREATED="1540471638660" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="" ID="ID_156682419" CREATED="1540471649319" MODIFIED="1543184658803">
+<font NAME="Calibri" SIZE="12"/>
+<hook URI="diagonal%20matrix.jpg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="\latex v = S" ID="ID_303299790" CREATED="1540471585132" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="" ID="ID_822134205" CREATED="1540471695026" MODIFIED="1543189458320">
+<font NAME="Calibri" SIZE="12"/>
+<hook URI="eigenvector%20matrix.jpg" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="All eigenvectors are stacked according to the order of eigenvalues" ID="ID_605630604" CREATED="1540471711271" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="\latex AS = \lambda S" ID="ID_1566382495" CREATED="1540471507201" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex if \lambda is diagonal you can write" ID="ID_124632396" CREATED="1540473230845" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex AS = S\lambda" ID="ID_1492823824" CREATED="1540473490776" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="\latex \lambda = S^{-1}AS" ID="ID_1877093986" CREATED="1540473515773" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+</node>
+<node TEXT="\latex A = S\lambda S^{-1}" ID="ID_1559812385" CREATED="1540474536953" MODIFIED="1540474570870"/>
+</node>
+</node>
+<node TEXT="\latex A = S^{-1}\lambda S" ID="ID_152981505" CREATED="1540473932403" MODIFIED="1540473995768"/>
+</node>
+<node TEXT="not diagonizable A" ID="ID_1744419730" CREATED="1540471506639" MODIFIED="1540473879481">
+<font NAME="Calibri" SIZE="12"/>
+<node TEXT="using Jordan canonical form" ID="ID_1900718968" CREATED="1540475495432" MODIFIED="1540475506505"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="stability" POSITION="left" ID="ID_167369919" CREATED="1540476526280" MODIFIED="1540476530639">
+<edge COLOR="#00ffff"/>
+<node TEXT="complex roots" ID="ID_1874170405" CREATED="1540480882063" MODIFIED="1540480898197"/>
+</node>
+</node>
+</map>
